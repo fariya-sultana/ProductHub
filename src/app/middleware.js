61514797a7ec17dc2dae039b6080eed3ab2efc67
fariya-sultana <fariya.sultana.dev@gmail.com>
2025-08-22 16:01:09 +0600

    add middleware

---
+++ src/app/middleware.js
@@ -0,0 +1,25 @@
+// middleware.js
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth(
+    // Optional: custom redirect logic
+    function middleware(req) { },
+    {
+        callbacks: {
+            // Restrict only authenticated users
+            authorized: ({ token }) => {
+                console.log("Middleware token:", token) // 👈 check logs
+                return !!token
+            },
+
+        },
+        pages: {
+            signIn: "/login", // redirect here if not logged in
+        },
+    }
+)
+
+// Protect only these routes
+export const config = {
+    matcher: ["/dashboard/:path*"], // protects /dashboard and its sub-routes
+}
